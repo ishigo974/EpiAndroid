a8da7edf519b63e0108b627dd63866ac0839f244
Maxime Menigoz <maxime.menigoz@gmail.com>
2016-01-13 02:51:19 +0100

    remove navigation and add login form

--- app/src/main/java/com/example/menigo_m/epiandroid/MainActivity.java
+++ app/src/main/java/com/example/menigo_m/epiandroid/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.menigo_m.epiandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button submit = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submit = (Button) findViewById(R.id.submit_button);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO LOGIN
+            }
+        });
+
     }
+
 }
