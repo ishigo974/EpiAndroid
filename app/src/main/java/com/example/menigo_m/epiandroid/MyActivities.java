7a6d10e5bd5231b05ec6fba3852dc86982ebcec6
Maxime Menigoz <maxime.menigoz@gmail.com>
2016-01-16 16:09:47 +0100

    remove menu

--- app/src/main/java/com/example/menigo_m/epiandroid/MyActivities.java
+++ app/src/main/java/com/example/menigo_m/epiandroid/MyActivities.java
@@ -1,33 +1,9 @@
 package com.example.menigo_m.epiandroid;
 
-import android.content.Intent;
-import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 /**
  * Created by menigo_m on 16/01/16.
  */
 public abstract class MyActivities extends AppCompatActivity {
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                Intent preferenceActivity = new Intent(MyActivities.this, PreferenceActivity.class);
-                startActivity(preferenceActivity);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
