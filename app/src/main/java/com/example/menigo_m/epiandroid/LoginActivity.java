012b0cb1699e49fc86a31f465e73a5eb264b36e5
Kevin Lopes <kevin.lopes@epitech.eu>
2016-01-19 18:19:44 +0100

    icon

--- app/src/main/java/com/example/menigo_m/epiandroid/LoginActivity.java
+++ app/src/main/java/com/example/menigo_m/epiandroid/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,10 +26,10 @@ public class LoginActivity extends MyActivities {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        if (preferences.getString("token", "Token not found") != "Token not found")
+        if (preferences.contains("token"))
         {
             Intent homeActivity = new Intent(LoginActivity.this, HomeActivity.class);
             startActivity(homeActivity);
