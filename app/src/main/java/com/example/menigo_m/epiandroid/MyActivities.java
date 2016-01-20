59a32f97451344a732eeef47e2de5a3f4dada45a
Maxime Menigoz <maxime.menigoz@gmail.com>
2016-01-20 16:25:47 +0100

    layouts

--- app/src/main/java/com/example/menigo_m/epiandroid/MyActivities.java
+++ app/src/main/java/com/example/menigo_m/epiandroid/MyActivities.java
@@ -1,12 +1,12 @@
 package com.example.menigo_m.epiandroid;
 
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 
 import com.android.volley.RequestQueue;
 
 /**
  * Created by menigo_m on 16/01/16.
  */
-public abstract class MyActivities extends FragmentActivity {
+public abstract class MyActivities extends AppCompatActivity {
     static protected RequestQueue queue = null;
 }
