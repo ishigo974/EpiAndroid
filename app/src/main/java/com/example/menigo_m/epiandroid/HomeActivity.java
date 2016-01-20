d20a7f6862c69e85f9abc17e048ee29a7d3d014c
Maxime Menigoz <maxime.menigoz@gmail.com>
2016-01-20 13:27:25 +0100

    add menu bar

--- app/src/main/java/com/example/menigo_m/epiandroid/HomeActivity.java
+++ app/src/main/java/com/example/menigo_m/epiandroid/HomeActivity.java
@@ -3,6 +3,8 @@ package com.example.menigo_m.epiandroid;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class HomeActivity extends MyActivities {
@@ -14,5 +16,16 @@ public class HomeActivity extends MyActivities {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         Toast.makeText(getApplicationContext(), preferences.getString("token", "Token not found"), Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), preferences.getString("login", "Login not found"), Toast.LENGTH_LONG).show();
+
+        String[] menus = new String[]
+                {
+                        "Home",
+                        "Modules",
+                        "Projects"
+                };
+
+        ListView listView = (ListView) findViewById(R.id.menu_elements);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(HomeActivity.this, R.layout.my_list_item, menus);
+        listView.setAdapter(adapter);
     }
 }
